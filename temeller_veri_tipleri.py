8d61cef5894b5975ef0325b744c6821b774aba12
Reyyan Ceyda Can <156205708+ReyCeyC@users.noreply.github.com>
2025-10-26 14:10:21 +0300

    Create temeller_veri_tipleri.py

---
+++ temeller_veri_tipleri.py
@@ -0,0 +1,10 @@
+isim = input("Adınızı girin: ")
+yas = int(input("Yaşınızı girin: "))
+print(f"Merhaba {isim}, {yas} yaşındasınız.")
+sayi1 = float(input("Bir sayı girin: "))
+sayi2 = float(input("Bir sayı daha girin: "))
+toplam = sayi1 + sayi2
+print("Girdiğiniz sayıların toplamı:", toplam)
+re_sit_mi = yas >= 18
+print("Reşit misiniz?", re_sit_mi)
+print(f"Adınız {len(isim)} karakter uzunluğunda.")
